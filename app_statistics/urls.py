b1dff756cf48ceb486e49bc3688c306e96120b9c
AlmasZh <almas.zh22@gmail.com>
2025-12-28 20:35:21 +0500

    feat: update environment configuration, enhance URL handling, and improve views for statistics app

--- app_statistics/urls.py
+++ app_statistics/urls.py
@@ -5,5 +5,5 @@ app_name = "app_statistics"
 
 urlpatterns = [
     path('', views.statistics, name='statistics'),
-    # path('info', views.dataframe_info, name='pd_info'),
+    path('info', views.dataframe_info, name='pd_info'),
 ]
--- app_statistics/views.py
+++ app_statistics/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+import os
+import json
+
+
+STATS_API_URL = os.getenv("STATISTICS_API_URL", "http://localhost:8080")
 
 def statistics(request):
     return HttpResponse(content="this page is currently unavailable")
+
+def dataframe_info(request):
+    data = requests.get(f'{STATS_API_URL}/info')
+    data_dict = json.loads(data.content)
+    return render(request, "pd_info.html", data_dict)
